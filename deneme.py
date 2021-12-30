1990cc8c9aca1583cbbb595eb284c3635967934b
EAGLEEARTH <ufukcicek199@gmail.com>
2021-12-30 18:28:35 +0300

    first commit

---
+++ deneme.py
@@ -0,0 +1,168 @@
+from typing import Counter
+from playwright.sync_api import sync_playwright
+import hashlib
+from bs4 import BeautifulSoup
+import sys
+import time
+import datetime
+import requests
+import re
+import math
+import os
+import json
+import pickle
+import schedule
+import time
+
+user_dir = '/tmp/playwright'
+
+def job():
+    print("Working....")
+    with open("./read_file.txt","r", encoding="utf-8") as f:
+        text = f.read()
+        read_file(text)
+
+def read_file(text):
+    
+    if text:
+        text_split = text.split(",")
+        for link in text_split:
+            if "http" in link or "www" in link:
+                if link:
+                    open_browser(link)
+            else:
+                text = link
+                link = "https://www.google.com.tr/?hl=tr"
+                open_browser(link,text)
+
+def open_browser(link,text=None):
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch_persistent_context(user_dir, headless=False)
+        page = browser.new_page()
+        
+        #page.wait_for_timeout(30000000)
+        try:    
+            if link == "https://www.google.com.tr/?hl=tr":
+                page.goto(link)
+                page.wait_for_load_state()
+                # bekletme koymak için
+                page.wait_for_timeout(3000)
+                page_content = page.content()
+                soup = BeautifulSoup(page_content, 'html.parser')
+                print("scroll not working")
+                input_text(page,text)
+                page_enter_click(page, text)
+                href_selector_all(page)
+                
+            else:
+                page.goto(link)
+                page.wait_for_load_state()
+                # bekletme koymak için
+                page.wait_for_timeout(3000)
+                page_content = page.content()
+                soup = BeautifulSoup(page_content, 'html.parser')
+                scroll_down_page(page)
+                page.wait_for_timeout(visit_page_time)
+                
+            browser.close()
+        except Exception as a:
+            print(a)
+
+def input_text(page, text=None):
+    selector = "input.gLFyf.gsfi"
+    page.fill(selector, text)
+    page.wait_for_timeout(3000)
+      
+def page_enter_click(page, text=None):
+    page.keyboard.press("Enter")   
+    page.wait_for_timeout(3000)
+    
+def page_escape_click(page, text=None):
+    page.keyboard.press("Escape")   
+    page.wait_for_timeout(3000)
+    
+def href_selector_all(page):
+    
+    selector = href_selector_click
+    search_link_ = page.query_selector_all(selector)
+    count_link = len(search_link_)
+    href_count = search_page_visi_count
+    if href_count <= count_link:
+        for link_count in range(0, href_count):
+            
+            item = page.query_selector_all(selector)[link_count]
+            if item:
+                print("item True")
+                item.click()
+                page.wait_for_timeout(3000)
+                page_escape_click(page)
+                page.reload()
+                page.set_default_navigation_timeout(3000)
+                scroll_down_page(page)
+                click_button_action(page)
+                page.wait_for_timeout(visit_page_time)
+                #page.go_back()
+            
+
+
+def scroll_down_page(page):
+    scroll_top = page.evaluate("window.scrollY")
+    scroll_height = page.evaluate(f"document.querySelector('body').scrollHeight")
+    scroll_pixel = 500
+    selector = "body"
+    wait_timeout = True
+    while (scroll_height - scroll_top) > (scroll_pixel * 3):
+        page.evaluate(f"window.scrollBy(0,{scroll_pixel})")
+        if wait_timeout:
+            page.wait_for_timeout(scrolling_time)
+        scroll_top = page.evaluate("window.scrollY")
+        scroll_height = page.evaluate(f"document.querySelector('{selector}').scrollHeight")
+        page.wait_for_timeout(3000)
+    return scroll_top
+
+
+def click_button_action(page): 
+    selector = "button"
+    button_selector = page.query_selector_all(selector)
+    button_count = len(button_selector)
+    print("button_sount",button_count)
+    if button_count > 3:
+        for button in range(0,3):
+            print(button,"----")
+            try:
+                if page.evaluate(f"document.querySelectorAll('button')[{button}].click();"):
+                    page.wait_for_timeout(6000)  
+                    page.go_back()
+            except:
+                print("next button not found")
+                page.wait_for_timeout(6000)  
+                page.go_back()  
+
+
+def options_json():
+    with open("./options.json","r") as f:
+        options_data = json.loads(f.read()) 
+    return options_data
+
+if __name__ == "__main__":
+
+    
+    options_data = options_json()
+
+    working_time = options_data.get("options").get("working_time")
+    visit_page_time = options_data.get("options").get("page_visit_time")
+    scrolling_time = options_data.get("options").get("scrolling_time")
+    search_page_visi_count = options_data.get("options").get("search_page_visi_count")
+    href_selector_click = options_data.get("options").get("href_selector_click")
+    job()
+    # schedule.every(working_time).minutes.do(job)
+    # print("hello")
+    # while True:
+    #     if schedule.run_pending():
+    #         time.sleep(1)
+
+        
+        
